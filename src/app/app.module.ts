42e9cbc1bb091a7bcc54cae408af9e1b84fe6e0d
Julien-Wiegandt <julienwiegandt@gmail.com>
2021-04-23 23:44:09 +0200

    initialize needed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
